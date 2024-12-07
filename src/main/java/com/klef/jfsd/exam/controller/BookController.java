e01232057f4d9975fa25daabe0d9e163f3262ed3
Manireddy7784 <2200032061cseh@gmail.com>
2024-12-07 10:50:19 +0530

    Add files via upload

---
+++ src/main/java/com/klef/jfsd/exam/controller/BookController.java
@@ -0,0 +1,24 @@
+package com.klef.jfsd.exam.controller;
+
+import com.klef.jfsd.exam.model.Book;
+import com.klef.jfsd.exam.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+    @Autowired
+    private BookService bookService;
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book bookDetails) {
+        try {
+            Book updatedBook = bookService.updateBook(id, bookDetails);
+            return ResponseEntity.ok(updatedBook);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
